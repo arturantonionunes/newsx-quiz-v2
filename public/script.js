2fff39cd25eab14f10358f5d629b75e01cc29f2e
Artur Nunes <arturantoniojj@gmail.com>
2025-06-05 17:48:36 -0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -21,25 +21,32 @@ document.addEventListener("DOMContentLoaded", () => {
       articles = data;
       console.log(articles);
       
-      // Verificar se há ID na URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const articleIdFromUrl = urlParams.get('id');
+    // Verificar se há ID na URL
+      const urlParams = new URLSearchParams(window.location.search);
+      const articleIdFromUrl = urlParams.get('id');
 
-    let initialArticle = null;
-    
-    // Se houver ID na URL, encontrar artigo correspondente
-    if (articleIdFromUrl) {
-      initialArticle = articles.find(article => article.id == articleIdFromUrl);
-    }
-    
-    // Se não encontrou, usar o último artigo
-    if (!initialArticle) {
-      initialArticle = articles[articles.length - 1];
-    }
+      let initialArticle = null;
+      let initialIndex = 0;
+      
+      // Se houver ID na URL, encontrar artigo correspondente
+      if (articleIdFromUrl) {
+        initialArticle = articles.find(article => article.id == articleIdFromUrl);
+        
+        // Se encontrou o artigo, obter seu índice
+        if (initialArticle) {
+          initialIndex = articles.findIndex(article => article.id == articleIdFromUrl);
+        }
+      }
+      
+      // Se não encontrou, usar o último artigo
+      if (!initialArticle) {
+        initialArticle = articles[articles.length - 1];
+        initialIndex = articles.length - 1;
+      }
 
-    // Encontrar o índice do artigo inicial
-    currentArticleIndex = articles.findIndex(article => article.id === initialArticle.id);
-    updateCard(initialArticle);
+      currentArticleIndex = initialIndex;
+      updateCard(initialArticle);
+
 
       const imagesContainer = document.getElementById("imagesContainer");
 
